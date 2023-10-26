8b1f7ef5f3ea84906cd34410c7404669e80d7dcb
zixun <zixun@berkeley.edu>
2023-10-25 20:24:35 -0700

    added example file and submodules

--- Xconfig.sh
+++ Xconfig.sh
@@ -1,3 +1,3 @@
-sudo nvidia-xconfig -a --use-display-device=None --virtual=1280x1024
+sudo nvidia-xconfig -a --virtual=1920x1080
 echo -e 'Section "ServerFlags"\n\tOption "MaxClients" "2048"\nEndSection\n' \
-    | sudo tee /etc/X11/xorg.conf.d/99-maxclients.conf
+    | sudo tee /usr/share/X11/xorg.conf.d/99-maxclients.conf

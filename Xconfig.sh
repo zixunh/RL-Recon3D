204b691e8c6abd7dacf3e4d28d77e59a82157533
zixun <zixun@berkeley.edu>
2023-10-25 20:37:48 -0700

    remove headless solution

--- Xconfig.sh
+++
@@ -1,3 +0,0 @@
-sudo nvidia-xconfig -a --virtual=1920x1080
-echo -e 'Section "ServerFlags"\n\tOption "MaxClients" "2048"\nEndSection\n' \
-    | sudo tee /usr/share/X11/xorg.conf.d/99-maxclients.conf
